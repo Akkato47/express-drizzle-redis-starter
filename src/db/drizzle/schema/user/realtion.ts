df552b803090d1f4c23a54fdf5b6a0c8423f8af8
kato <unknown.255.galactic.acc@gmail.com>
2025-02-05 12:42:35 +0500

    logical fixes

--- src/db/drizzle/schema/user/realtion.ts
+++ src/db/drizzle/schema/user/realtion.ts
@@ -3,15 +3,15 @@ import { relations } from 'drizzle-orm';
 import { files, images } from '../media/schema';
 import { userProfle, users } from './schema';
 
-export const userRelations = relations(users, ({ one }) => ({
-  userProfleRelation: one(userProfle)
+export const userRelations = relations(users, ({ one, many }) => ({
+  userProfleRelation: one(userProfle),
+  imagesRelation: many(images),
+  fileRelation: many(files)
 }));
 
-export const userProfileRelations = relations(userProfle, ({ one, many }) => ({
+export const userProfileRelations = relations(userProfle, ({ one }) => ({
   usersRelation: one(users, {
     fields: [userProfle.userUid],
     references: [users.uid]
-  }),
-  imagesRelation: many(images),
-  fileRelation: many(files)
+  })
 }));
